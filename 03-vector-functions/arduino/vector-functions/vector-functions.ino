c4d47deaa2a6a7c92667719adc954dfcb018567a
Michael Cheich <mcheich@gmail.com>
2022-10-24 12:01:16 -0400

    adjusted elementwise multiplication, added custom assert for assetListAlmostEqual

---
+++ 03-vector-functions/arduino/vector-functions/vector-functions.ino
@@ -0,0 +1,14 @@
+float elementwise_multipication() {
+  
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+}
--- 03-vector-functions/python/vector-functions.py
+++ 03-vector-functions/python/vector-functions.py
@@ -2,17 +2,17 @@ import unittest
 
 
 # Elementwise Multiplication
+# Takes 2 cevtors of teh same length and returns a single vector of their Hadamard product
 def elementwise_multiplication(vec_a, vec_b):
 
     assert(len(vec_a) == len(vec_b))
 
-    output = 0
+    output = []
 
     for i in range(len(vec_a)):
-        output += vec_a[i] * vec_b[i]
+        output.append(vec_a[i] * vec_b[i])
 
     return output
-
 
 
 class TestVectorFunctions(unittest.TestCase):
@@ -21,7 +21,14 @@ class TestVectorFunctions(unittest.TestCase):
         vec_a = [0.1, 0.2, 0]
         vec_b = [8.5, 0.65, 1.2]
         result = elementwise_multiplication(vec_a, vec_b)
-        self.assertAlmostEqual(result, 0.98)
+        self.assertListAlmostEqual(result, [0.85, 0.13, 0], 5)
+          
+
+    def assertListAlmostEqual(self, list1, list2, tolerance):
+        self.assertEqual(len(list1), len(list2))
+        for a, b in zip(list1, list2):
+            self.assertAlmostEqual(a,b,tolerance)
+
 
 if __name__ == '__main__':
     unittest.main()
