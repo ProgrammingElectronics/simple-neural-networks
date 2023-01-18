de9e203ecd7e10f9a3ee50c5d3985350d0f5ec36
Michael Cheich <mcheich@gmail.com>
2023-01-18 06:47:45 -0500

    upto training

--- mnist/mnist__all_the_things.py
+++ mnist/mnist__all_the_things.py
@@ -20,19 +20,37 @@ def create_embedding(val):
 
 # Data Transformation
 training_size = 1000
-labels = np.array([create_embedding(y) for y in y_train[:training_size]]).reshape((training_size,10))
-assert labels.shape == (training_size, 10) , f"Expect (1000, 10), actual {labels.shape}"
 
+## Training Data
+labels = np.array([create_embedding(y) for y in y_train[:training_size]]).reshape((training_size,10))
+images = x_train[:training_size].reshape(training_size,784) / 255
+
+assert labels.shape == (training_size, 10) , f"Expect (1000, 10), actual {labels.shape}"
+assert images.shape == (training_size, 784) , f"Expect (1000, 784, actual {images.shape}"
+
+## Test Data
+test_labels = np.array([create_embedding(y) for y in y_test]).reshape((len(y_test),10))
+test_images = x_test.reshape(len(x_test),784) / 255
+
+assert test_labels.shape == (len(y_test), 10), f"Expect (10000, 10), actual {test_labels.shape}"
+assert test_images.shape == (len(x_test), 784) , f"Expect (1000, 784, actual {test_images.shape}"
 
 # Controls
-
+alpha = 0.1
+epochs = 300
+mini_batch_size = 100
 
 # Network Architecture
+hidden_size = 40
 
 # Weights
+w_0_1 = 2 * np.random.random((784, hidden_size)) - 1
+w_1_2 = 2 * np.random.random((hidden_size, 10)) - 1
 
 # Training
+for epoch in range(epochs):
 
+    pass
     # Predict
 
     # Mini-batch
