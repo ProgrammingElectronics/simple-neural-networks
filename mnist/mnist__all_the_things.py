8fd73f66b4d94702bc72fb8d623b2239fd7074ed
Michael Cheich <mcheich@gmail.com>
2023-02-07 06:45:37 -0500

    tanh pract

--- mnist/mnist__all_the_things.py
+++ mnist/mnist__all_the_things.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plot
 from keras.datasets import mnist
 
+# Random
+np.random.seed(1)
+
 # Activation Functions
 def relu(x):
     return (x >= 0) * x
@@ -37,36 +40,44 @@ assert test_images.shape == (len(x_test), 784) , f"Expect (1000, 784, actual {te
 
 # Controls
 alpha = 0.1
-epochs = 1
+epochs = 300
 mini_batch_size = 100
+print_freq = 10
+graph_array_size = int((epochs/print_freq)) + 1
 
 # Network Architecture
-hidden_size = 40
+hidden_size = 100
 
 # Weights
-w_0_1 = 2 * np.random.random((784, hidden_size)) - 1
-w_1_2 = 2 * np.random.random((hidden_size, 10)) - 1
+w_0_1 = 0.2 * np.random.random((784, hidden_size)) - 0.1
+w_1_2 = 0.2 * np.random.random((hidden_size, 10)) - 0.1
 
-# Training
-for epoch in range(epochs):
+train_err_array = np.zeros((graph_array_size))
+train_acc_array = np.zeros((graph_array_size))
+test_err_array = np.zeros((graph_array_size))
+test_acc_array = np.zeros((graph_array_size))
 
+## Begin training and testing ##################################
+for epoch in range(epochs + 1):
+
+## Training ##################################
     # Metrics
-    error = 0
-    correct_count = 0
+    train_error = 0.0
+    train_correct_count = 0
 
     # Mini-batch
     for i in range(int(len(labels)/mini_batch_size)):
         
         # Mini Batch
-        images_mini_batch = images[i * mini_batch_size:i * mini_batch_size + mini_batch_size]
-        labels_mini_batch = labels[i * mini_batch_size:i * mini_batch_size + mini_batch_size]
+        images_mini_batch = images[i * mini_batch_size : i * mini_batch_size + mini_batch_size]
+        labels_mini_batch = labels[i * mini_batch_size : i * mini_batch_size + mini_batch_size]
 
         # Drop Out Mask
         drop_out_mask = np.random.randint(2, size=(mini_batch_size,hidden_size))
 
         # Predict #
         l0 = images_mini_batch
-        l1 = relu(l0.dot(w_0_1)) * drop_out_mask
+        l1 = relu(l0.dot(w_0_1)) * drop_out_mask * 2 # 1/2 of nodes are dropped out, so increase signal by 2 during training
         l2 = l1.dot(w_1_2)
         
         assert l0.shape == (mini_batch_size,784) , f"Expect ({mini_batch_size}, 784), actual {l0.shape}" 
@@ -75,26 +86,56 @@ for epoch in range(epochs):
 
         # Compare #
         # Error and Accuracy
-        error += np.sum((l2 - labels_mini_batch) ** 2)
+        train_error += np.sum((l2 - labels_mini_batch) ** 2)
         
         for i in range(mini_batch_size):
-            correct_count += np.argmax(labels_mini_batch[i]) == np.argmax(l2[i])
+            train_correct_count += np.argmax(labels_mini_batch[i]) == np.argmax(l2[i])
             
         # Update Weights
-        l2_delta = (l2 - labels_mini_batch) # prediction - actual
-        l1_delta = l2_delta.dot(w_1_2.T)*relu_deriv(l1) 
+        l2_delta = (l2 - labels_mini_batch) / mini_batch_size # prediction - actual
+        l1_delta = l2_delta.dot(w_1_2.T)*relu_deriv(l1) * drop_out_mask 
         
         l2_wt_delta = l2_delta.T.dot(l1)
-        l1_wt_delta = l1_delta.T.dot(l0)
+        l1_wt_delta = l1_delta.T.dot(l0) 
 
         w_1_2 -= l2_wt_delta.T * alpha
         w_0_1 -= l1_wt_delta.T * alpha
-# Testing
 
-    # Predict
+## Testing ##################################
+    
+    # Metrics
+    test_error = 0
+    test_correct_count = 0
 
-    # Compare
+    for test_label, test_image in zip(test_labels, test_images):
+    
+        # Predict
+        l0 = test_image
+        l1 = relu(l0.dot(w_0_1))
+        l2 = l1.dot(w_1_2)
 
-# Printing Results
+        # Compare
+        test_error += np.sum((l2 - test_label)**2)
+        test_correct_count += np.argmax(l2) == np.argmax(test_label)
 
+    # Printing and Graphing Results
+    train_err_percent = train_error / float(len(labels))
+    train_acc_percent = train_correct_count / float(len(labels))
+    test_err_percent = test_error / float(len(test_labels))
+    test_acc_percent = test_correct_count / float(len(test_labels))
+
+    if(epoch % print_freq == 0):
+        # Print
+        print(epoch, "Train_Err", "{:.3f}".format(train_err_percent), \
+                     "Train_Acc", "{:.3f}".format(train_correct_count / float(len(labels))), \
+                     "Test_Err", "{:.3f}".format(test_error / float(len(test_labels))), \
+                     "Test_Acc", "{:.3f}".format(test_correct_count / float(len(test_labels))))
+
+        # Graph
+        index = int(print_freq/print_freq)
+        train_err_array[index] = train_err_percent
+        train_acc_array[index] = train_acc_percent
+        test_err_array[index] = test_err_percent
+        test_acc_array[index] = test_acc_percent
+    
 # Graphing Results
---
+++ mnist/mnist_tanh.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+
+np.random.seed(1)
+
+## Functions ################################
+
+# Activations
+def tanh(x):
+
+    return np.tanh(x)
+
+def tanh_deriv(x):
+
+    return 1 - (x ** 2)
+
+def softmax(x):
+
+    temp = np.exp(x)
+    return temp / np.sum(temp) 
+
+# Embeddings
+def embedding(x):
+
+    embedding = np.zeros((1,10))
+    embedding[0,x] = 1
+
+    return embedding
+
+## Data
+# Import Data
+(x_train, y_train), (x_test, y_test) = mnist.load_data() 
+
+# Transform Data
+num_train_samples = 1000
+
+images = np.reshape(x_train[:num_train_samples] / 256, (num_train_samples, 784)) 
+labels = np.reshape([embedding(y) for y in y_train[0:num_train_samples]], (num_train_samples, 10))
+
+test_images = np.reshape(x_test / 256, (len(x_test), 784))
+test_labels = np.reshape([embedding(y) for y in y_test], (len(y_test), 10))
+
+## Controls
+alpha = 0.001
+epochs = 1
+
+mini_batch_size = 100
+num_mini_batches = int(len(images)/mini_batch_size)
+
+## Architecture
+hidden_layer_size = 40
+
+## Weights
+wt_0_1 = 0.02 * np.random.random((784, hidden_layer_size)) - 0.01
+wt_1_2 = 0.2 * np.random.random((hidden_layer_size, 10)) - 0.1
+
+## Reporting
+accuracy_plot = np.zeros((epochs)) 
+
+
+## Training
+for epoch in range(epochs):
+    
+    correct_cnt = 0
+
+    for mini_batch in range(num_mini_batches):
+        
+        start_batch = mini_batch * mini_batch_size
+        stop_batch = mini_batch * mini_batch_size + mini_batch_size
+        drop_out_mask = np.random.randint(2,size=(mini_batch_size, hidden_layer_size)) 
+
+        ## Predict ######################################
+        l0 = images[start_batch:stop_batch]
+        l1 = tanh(l0.dot(wt_0_1)) * drop_out_mask * 2
+        l2 = softmax(l1.dot(wt_1_2))
+        
+        ## Compare ######################################
+        # Error - TBD for softmax...
+        # Accuracy
+        batch_labels = labels[start_batch:stop_batch]
+
+        for i in range(mini_batch_size):
+            
+            correct_cnt += np.argmax(l2[i]) == np.argmax(batch_labels[i])
+        
+        ## Learn ######################################
+        # Deltas
+        l2_delta = (l2 - batch_labels)
+        l1_delta = l2_delta.dot(wt_1_2.T) * tanh_deriv(l1) * drop_out_mask
+
+        # Weight Deltas
+        l2_wt_delta = l2_delta.T.dot(wt_0_1)
+        l1_wt_delta = l1_delta.T.dot(wt_1_2)
+
+        # Adjust Weight
+        wt_1_2 += l2_wt_delta * alpha
+        wt_0_1 += l1_wt_delta * alpha
+
+## Testing
+
+    ## Predict
+
+    ## Compare
+
+    # Error
+
+    # Accuracy
+
+## Visualize
+
+# Graph Error
+# Print Error
+
+
+
