8e9a1dbd15ed96ceccc37455391d10f2798f1f56
Michael Cheich <mcheich@gmail.com>
2023-02-16 06:08:34 -0500

    tanh working

--- mnist/mnist_tanh.py
+++ mnist/mnist_tanh.py
@@ -57,6 +57,7 @@ wt_1_2 = 0.2 * np.random.random((hidden_layer_size, 10)) - 0.1
 ## Reporting
 freq = 10 
 train_accuracy_plot = np.zeros((int(epochs/freq))) 
+test_accuracy_plot = np.zeros((int(epochs/freq))) 
 
 ## Training
 for epoch in range(epochs):
@@ -85,36 +86,47 @@ for epoch in range(epochs):
 
         ## Learn ######################################
         # Deltas
-        l2_delta = (l2 - batch_labels) / (mini_batch_size * l2.shape[0])
+        l2_delta = (l2 - batch_labels) / (mini_batch_size * l2.shape[0]) # dividing by l2.shape[0] is part of derivative of softmax
         l1_delta = l2_delta.dot(wt_1_2.T) * tanh_deriv(l1) * drop_out_mask
 
         # Adjust Weight
         wt_1_2 -= l1.T.dot(l2_delta) * alpha
         wt_0_1 -= l0.T.dot(l1_delta) * alpha
     
-## Testing ##
+    ## Testing ##
+    test_correct_cnt = 0
 
-    ## Predict
+    for test_image, test_label in zip(test_images, test_labels):
 
-    ## Compare
+        drop_out_mask = np.random.randint(2, size=(1,hidden_layer_size))
 
-    # Error
-
-    # Accuracy
+        ## Predict
+        l0 = test_image
+        l1 = tanh(l0.dot(wt_0_1)) * drop_out_mask * 2
+        l2 = softmax(l1.dot(wt_1_2))
+        
+        ## Compare
+        # Accuracy
+        test_correct_cnt += int(np.argmax(l2) == np.argmax(test_label))
 
     ## Visualize
     # Print Error
     if(epoch % freq == 0):
         train_accuracy_plot[int(epoch/freq)] = correct_cnt / float(len(labels))
         print(epoch, "Train Acc: ", train_accuracy_plot[int(epoch/freq)])
+        test_accuracy_plot[int(epoch/freq)] = test_correct_cnt / float(len(test_labels))
+        print(epoch, "Test Acc: ", test_accuracy_plot[int(epoch/freq)])
         
         
 
 # Graph Error
-
-fig, ax = plt.subplots()
-plt.ylabel("Train Accuracy")
-ax.plot(train_accuracy_plot)
+fig, ax = plt.subplots() # an empty figure with no Axes
+ax.plot(range(len(train_accuracy_plot)), train_accuracy_plot, label=('train'))
+ax.plot(range(len(test_accuracy_plot)), test_accuracy_plot, label=('test'))
+ax.set_xlabel('epochs')
+ax.set_ylabel('accuracy')
+ax.set_title("mnist all the things")
+ax.legend()
 plt.show()
 
 
